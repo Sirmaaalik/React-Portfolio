e44b9056bc21c3af39b6c7040e09a2cf20eea6a8
Sirmaaalik <malikrharris@gmail.com>
2021-12-11 04:20:25 -0500

    Changing the pictures and editing all the javascript.  A friend gave me a template and assisted me on this

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     return (
         <footer className="footerStyles">
             <a href="https://www.linkedin.com/mwlite/in/catherine-clifford-1b09672111" target="_blank"><img className="imageStyle" src={linkedInIMG} /></a>
-            <a href="https://github.com/Flying-dink" target="_blank"><img className="imageStyle" src={githubIMG} /></a>
+            <a href="https://github.com/Sirmaaalik" target="_blank"><img className="imageStyle" src={githubIMG} /></a>
             <a href="mailto:skydoll96@gmail.com"><img className="imageStyle" src={emailIMG} /></a>
         </footer>
     )
